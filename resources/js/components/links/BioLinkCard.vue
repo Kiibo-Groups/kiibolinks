a919effdcf1fc8d9b68707d9a8f78b21b5ffadf9
Diego C/G <diego.castilloguajardo@gmail.com>
2023-10-10 23:18:56 -0300

    Add biolink card

--- resources/js/components/links/BioLinkCard.vue
+++ resources/js/components/links/BioLinkCard.vue
@@ -2,27 +2,33 @@
     <article class="bg-white p-3 rounded-3xl max-w-md w-full">
         <div class="relative">
             <img
-                src="https://placekitten.com/600/200"
+                src="assets/themes/theme-demo/desert.png"
                 alt="Banner Profile"
                 class="w-full rounded-t-lg"
             />
             <img
-                src="https://placekitten.com/150/150"
                 alt="Profile Picture"
                 class="absolute bottom-0 left-2/4 transform -translate-x-1/2 translate-y-1/2 w-24 h-24 rounded-full border-4 border-white"
             />
         </div>
 
         <div class="flex items-center justify-center mt-14 mb-2">
-            <h2 class="text-xl font-bold text-gray-800">DCGX</h2>
+            <h2 class="text-xl font-bold text-gray-800">{{ name }}</h2>
         </div>
 
         <div class="flex items-center my-2 justify-center space-x-4">
-            <div href="#" class="text-gray-900">
+            <a
+                :href="`/dashboard/biolink/analytics/${id}/overview`"
+                class="text-gray-900"
+            >
                 <i class="fa-solid fa-chart-line"></i>
-                0 Visitas
-            </div>
-            <a href="#" class="text-blue-500 no-underline hover:underline">
+                {{ visitedCount }} Visitas
+            </a>
+            <a
+                :href="urlName"
+                :id="`visitLink-${id}`"
+                class="text-blue-500 no-underline hover:underline"
+            >
                 Visitar link
             </a>
         </div>
@@ -55,9 +61,15 @@
 </template>
 
 <script setup>
-const props = defineProps([]);
+const props = defineProps(["id", "name", "urlName", "visitedCount"]);
+
+console.log(props);
 
 const redirectToEditLink = () => {
-    window.location.href = "/dashboard/biolink/user";
+    window.location.href = "/dashboard/biolink/" + props.urlName;
+};
+
+const visitLink = () => {
+    window.location.href = "/dashboard/biolink/" + props.urlName;
 };
 </script>
--- resources/js/components/links/CreateBioLinkButton.vue
+++ resources/js/components/links/CreateBioLinkButton.vue
@@ -1,6 +1,11 @@
 <template>
-    <button class="bg-white p-3 rounded-3xl max-w-md w-full">
-        EDITAR BOTON
+    <button
+        class="bg-white p-3 rounded-3xl max-w-md w-full flex-col flex items-center justify-center"
+    >
+        <div class="bg-gray-100 p-4 rounded-full mb-2">
+            <i class="fa-solid fa-plus text-3xl"></i>
+        </div>
+        <span class="text-lg font-medium mt-2"> Agregar BioLink </span>
     </button>
 </template>
 
